024a87e154bdd0f198c588d5379e1b0968b20300
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2023-07-22 17:38:19 +0300

    update Home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,6 +1,16 @@
+import '../Assets/CSS/Home.css';
+
 function Home() {
     return (
         <div>
+            <div className="container demo">
+                <div className="content">
+                    <div id="large-header" className="large-header">
+                        <canvas id="demo-canvas"></canvas>
+                        <h1 className="main-title"><span className="thin">Explore</span> Space</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
