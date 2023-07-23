a8e54969995276d889d6b76088fd6153b7b4ddd4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2023-07-23 23:37:55 +0300

    update home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,13 +3,13 @@ import '../Assets/CSS/Home.css';
 function Home() {
     return (
         <div>
-            <div className="container demo">
-                <div className="content">
-                    <div id="large-header" className="large-header">
-                        <canvas id="demo-canvas"></canvas>
-                        <h1 className="main-title"><span className="thin">Explore</span> Space</h1>
-                    </div>
-                </div>
+            <div id="header" className="header fixed-top">
+                <header>
+                    <div className="sticky-header bg-dark">Sticky Header</div>
+                    <div className="searchbar bg-light">Hidden Bar</div>
+                </header>
+
+                <div className="bg-dark">Sticky Bar</div>
             </div>
         </div>
     )
