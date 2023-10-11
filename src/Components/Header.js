e686cbeea235a3657c182547d55443d0e83162fd
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2023-10-11 21:21:30 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,22 @@
+import '../Assets/CSS/Header.css';
 
 function Header() {
 
   return (
     <div>
+      <nav>
+        <div className="menu">
+          <p>Webpage</p>
+          <button>Sign up</button>
+        </div>
+        <div className="backdrop"></div>
+      </nav>
+
+      <main>
+        <h1>Header</h1>
+        <p>Text goes here</p>
+        <a href="/" target="_blank"><button>Source</button></a>
+      </main>
     </div>
   )
 }
