b63a71f872945c38bcac4cb2ce18ebc485176a08
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2023-10-12 11:21:30 +0300

    edit header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,19 +4,21 @@ function Header() {
 
   return (
     <div>
-      <nav>
-        <div className="menu">
-          <p>Webpage</p>
-          <button>Sign up</button>
-        </div>
-        <div className="backdrop"></div>
-      </nav>
+      <div id='header-body'>
+        <nav id='header-nav'>
+          <div className="menu">
+            <p>Webpage</p>
+            <button id='header-btn'>Sign up</button>
+          </div>
+          <div className="backdrop"></div>
+        </nav>
 
-      <main>
-        <h1>Header</h1>
-        <p>Text goes here</p>
-        <a href="/" target="_blank"><button>Source</button></a>
-      </main>
+        <main id='header-main'>
+          <h1>Header</h1>
+          <p>Text goes here</p>
+          <a href="/" target="_blank"><button>Source</button></a>
+        </main>
+      </div>
     </div>
   )
 }
